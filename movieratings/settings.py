af4c2649de54e04404ef6af1b637f126a473b027
Alan R <alan.rominger@gmail.com>
2015-06-16 15:46:44 -0400

    bootstraps basic functionality

--- movieratings/settings.py
+++ movieratings/settings.py
@@ -124,7 +124,8 @@ BOOTSTRAP3 = {
     'base_url': '/static/bootstrap/',
 
     # The complete URL to the Bootstrap CSS file (None means no theme)
-    'theme_url': '/static/bootstrap/css/sandstone.css',
+#    'theme_url': '/static/bootstrap/css/sandstone.css',
+    'theme_url': '/static/bootstrap/css/bootstrap-theme.min.css',
 
     # Include jQuery with Bootstrap JavaScript (affects django-bootstrap3 template tags)
     'include_jquery': True,
